0294a79614226d4ef0872ac89abdb66a2030b8f5
h0za1fa <hozaifaco@gmail.com>
2025-11-16 22:23:50 +0500

    deving

--- quizzler/main.py
+++ quizzler/main.py
@@ -10,7 +10,6 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-
 quiz = QuizBrain(question_bank)
 window = QuizInterface(quiz)
 
@@ -18,4 +17,4 @@ window = QuizInterface(quiz)
 #     quiz.next_question()
 
 print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+print(f"Your 8 score was: {quiz.score}/{quiz.question_number}")
--- quizzler/ui.py
+++ quizzler/ui.py
@@ -13,11 +13,13 @@ class QuizInterface:
         self.score = Label(text='Score: 0', bg=THEME_COLOR, highlightthickness=0)
         self.score.grid(column=2, row=0, pady=20)
 
+        self.question = 'text'
         self.canvas = Canvas()
         self.canvas.grid(column=1, row=1, columnspan=2, padx=50, pady=50)
         self.canvas.config(height=250, width=300)
-        self.text = self.canvas.create_text(150, 125, text=self.next_question(), font=  ('Arial', 20, 'italic') )
-        
+        self.text = self.canvas.create_text(150, 125, text=self.question,width=280 , font=  ('Arial', 18, 'italic') )
+        self.next_question()
+
         self.true_image = PhotoImage(file='Quizzler/images/true.png')
         self.false_image = PhotoImage(file='Quizzler/images/false.png')
 
@@ -26,8 +28,9 @@ class QuizInterface:
         self.false_button = Button(image=self.false_image, highlightthickness=0)
         self.false_button.grid(column=1, row=2, padx=20, pady=20)
     
+        self.window.mainloop()
+
     def next_question(self):
         self.question = self.quiz.next_question()
-        self.canvas.itemconfig(self.text, Text=self.question)
+        self.canvas.itemconfig(self.text, text=self.question)
 
-        self.window.mainloop()
